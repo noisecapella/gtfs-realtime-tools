e0b13f6c7ba8b97249727030f587301a27a24213
George Schneeloch <bostonbusmap@gmail.com>
2014-09-07 10:34:27 -0400

    Bug fix

--- test_gtfs_map.py
+++ test_gtfs_map.py
@@ -2,15 +2,15 @@ from gtfs_map import GtfsMap
 import datetime
 import argparse
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("gtfs_path")
-	args = parser.parse_args()
-	gtfs_path = args.gtfs_path
+        parser = argparse.ArgumentParser()
+        parser.add_argument("gtfs_path")
+        args = parser.parse_args()
+        gtfs_path = args.gtfs_path
 
-	gtfs_map = GtfsMap(gtfs_path, False)
-	for row in gtfs_map.find_stop_times_for_date(datetime.datetime.now()):
-		print(row)
+        gtfs_map = GtfsMap(gtfs_path, False)
+        for row in gtfs_map.find_stop_times_for_datetime(datetime.datetime.now()):
+                print(row)
 
 
 if __name__ == "__main__":
-	main()
+        main()
